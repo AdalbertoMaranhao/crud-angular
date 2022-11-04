44e3044cfc2a341346c3b1cd5dfb7f51a5d3d6bd
AdalbertoMaranhao <limadalberto@gmail.com>
2022-11-04 16:19:54 -0300

    capturing data from api

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/cursos.json';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return  [
-      {_id: '1', name: 'Angular', category:'front-end'}
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }
